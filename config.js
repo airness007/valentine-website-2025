31e14a47a838636511e63ec36425c48dc1850cfa
airness007 <131955835+airness007@users.noreply.github.com>
2026-02-14 17:44:38 +0200

    Update valentineName and music URL in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Cherity",
+    valentineName: "Mon ange Lungile",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dssjgc476/video/upload/v1771006453/Eh_Joh_hmugvo.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dssjgc476/video/upload/v1771083754/Folded_spotdown.org_cyx3v6.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
